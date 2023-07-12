01dc1564fd726b1aec9cabd1c1f885e8c98e5011
Mathios16 <matheussalomao12m@gmail.com>
2023-07-12 12:48:45 -0300

    Exercicio 1 do Desafio

---
+++ eclipse-workspace/DesafioDeCodigo/src/main/java/desafioDeCodigos/Exercicio_1.java
@@ -0,0 +1,23 @@
+package desafioDeCodigos;
+
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Exercicio_1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		Stack<String> frase = new Stack<String>();
+	
+		System.out.println("Entre com uma frase: ");
+		for(String palavra:entrada.nextLine().split(" "))
+			frase.push(palavra);
+		
+		System.out.println("\nA frase de saida será: ");
+		while(!frase.isEmpty())
+			System.out.print(frase.pop()+" ");
+		
+		entrada.close();
+	}
+}
